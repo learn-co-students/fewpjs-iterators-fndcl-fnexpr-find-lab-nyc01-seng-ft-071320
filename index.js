716574704a58de3ca48ac9623765b6ef7d88aed9
redazures <jwu_nyc@yahoo.com>
2020-08-30 10:14:50 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+  
+superbowlWin = (record) => {
+  const result = record.find( record => record.result === "W" )
+  return result ? result.year : undefined
+}
